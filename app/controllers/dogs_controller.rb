11b9e2763feb7b605c04736592445406ed8a6692
eliva <eliaandreavasquez@gmail.com>
2019-05-22 16:41:37 -0500

    perros

---
+++ app/controllers/dogs_controller.rb
@@ -0,0 +1,23 @@
+class DogsController < ApplicationController
+  def index
+  	@dogs = Dog.all
+  end
+
+  def show
+  	@dog= Dog.find(params[:id])
+  end
+
+  def new
+  	@dog=Dog.new
+  end
+  def create
+  	dog=Dog.create(dog_params)
+
+  	redirect_to dogs_path
+  end
+
+  private
+  def dog_params
+  	params.require(:dog).permit(:name,:motto)
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+Dog.create(name:'Nala',motto:'Born to be wild')
+Dog.create(name:'Alex',motto:'calm as it can be')
+Dog.create(name:'Leroy',motto: 'life of the pawty')
+Dog.create(name:'Belle',motto:'Miss independent')
+
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
